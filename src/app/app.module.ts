5d16f68efec22b1ec8c1a9a7464f59a2b2f3fcf2
Rustique-Uwimpaye <rustique644@gmail.com>
2020-12-11 00:50:43 +0300

    updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { InvolvedComponent } from './involved/involved.component';
+import { WorkComponent } from './work/work.component';
+import { WhoweareComponent } from './whoweare/whoweare.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    InvolvedComponent,
+    WorkComponent,
+    WhoweareComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
