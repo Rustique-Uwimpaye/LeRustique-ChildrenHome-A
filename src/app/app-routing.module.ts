b1b3ba6b576aa8cf4348997152b9ffbe1c67e621
Rustique-Uwimpaye <rustique644@gmail.com>
2020-12-12 16:41:12 +0300

    updated more on app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { InvolvedComponent } from './involved/involved.component';
+import { LocationsComponent } from './locations/locations.component';
 import { WhoweareComponent } from './whoweare/whoweare.component';
 import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
   { path: 'whoweare', component: WhoweareComponent},
   { path: 'work', component: WorkComponent},
   { path: 'involved', component: InvolvedComponent},
+  { path: 'locations', component: LocationsComponent}
 ];
 
 @NgModule({
